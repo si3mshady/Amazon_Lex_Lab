b4708ad32b44e720e33bc97024793927b5ee6ed5
Elliott Arnold <46078321+si3mshady@users.noreply.github.com>
2023-10-20 07:48:56 -0500

    Update lex.tf
    switch from dental to food ordering

--- lex.tf
+++ lex.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1" # Set your desired AWS region
 }
 
-resource "aws_lex_bot" "dental_appointment_bot" {
-  name = "DentalAppointmentBot"
-  description = "Bot for Dental Appointments"
+resource "aws_lex_bot" "restaurant_order_bot" {
+  name = "RestaurantOrderBot"
+  description = "Bot for Food Ordering"
   create_version = false
   locale = "en-US"
 
@@ -21,7 +21,7 @@ resource "aws_lex_bot" "dental_appointment_bot" {
     max_attempts = 2
     message {
       content_type = "PlainText"
-      content = "I didn't understand you, what would you like to do?"
+      content = "I didn't understand you, what would you like to order?"
     }
   }
 
@@ -30,37 +30,45 @@ resource "aws_lex_bot" "dental_appointment_bot" {
   voice_id = "Salli"
 
   intent {
-    intent_name = aws_lex_intent.schedule_appointment.name
-    intent_version = "$LATEST" # Use $LATEST for built-in intents
+    intent_name = aws_lex_intent.order_food.name
+    intent_version = "$LATEST"
   }
 
   intent {
-    intent_name = aws_lex_intent.cancel_appointment.name
-    intent_version = "$LATEST" # Use $LATEST for built-in intents
+    intent_name = aws_lex_intent.cancel_order.name
+    intent_version = "$LATEST"
   }
 
   intent {
-    intent_name = aws_lex_intent.get_appointment_details.name
-    intent_version = "$LATEST" # Use $LATEST for built-in intents
+    intent_name = aws_lex_intent.get_order_status.name
+    intent_version = "$LATEST"
   }
 }
-resource "aws_lex_intent" "schedule_appointment" {
-  name             = "ScheduleAppointment"
-  description      = "Intent to schedule dental appointments"
-  create_version   = false
+
+resource "aws_lex_intent" "order_food" {
+  name = "OrderFood"
+  description = "Intent to order food from a restaurant"
+  create_version = false
 
   sample_utterances = [
-    "I want to schedule an appointment.",
-    "Can I book a dentist appointment.",
-    "I need to set up a dental checkup.",
-  ]
+    "I want to order food.",
+    "Can I get some food from your restaurant?",
+    "I'd like to place an order for delivery.",
+    "What's on the menu today?",
+    "How can I place an order for takeout?",
+    "I'm hungry. What can I order?",
+    "Do you have any specials for today?",
+    "I need to order some food for pickup.",
+    "Tell me about your food options.",
+    "I'm looking to get some food delivered."
+]
 
   confirmation_prompt {
     max_attempts = 2
 
     message {
       content_type = "PlainText"
-      content = "Sure, I can help you with that. Please provide the date and time for your appointment."
+      content = "Sure, I can help you with that. Please provide the items you want to order and any specific instructions."
     }
   }
 
@@ -76,61 +84,49 @@ resource "aws_lex_intent" "schedule_appointment" {
   }
 
   slot {
-    name        = "Date"
-    description = "The date for the appointment"
-    priority    = 1
-    slot_constraint   = "Required"
-    slot_type         = "AMAZON.DATE"
+    name = "OrderItems"
+    description = "The items to be ordered"
+    priority = 1
+    slot_constraint = "Required"
+    slot_type = "CUSTOM_FOOD_ITEMS" # You can define a custom slot type for food items
 
-    sample_utterances = ["I want to schedule an appointment on {Date}"]
-
-    value_elicitation_prompt {
-      max_attempts = 2
-
-      message {
-        content_type = "PlainText"
-        content = "On what date would you like to schedule the appointment"
-      }
-    }
-  }
-
-  slot {
-    name        = "Time"
-    description = "The time for the appointment"
-    priority    = 2
-    slot_constraint   = "Required"
-    slot_type         = "AMAZON.TIME"
-
-    sample_utterances = ["I want to schedule an appointment at {Time}"]
+    sample_utterances = ["I want to order {OrderItems}"]
 
     value_elicitation_prompt {
       max_attempts = 2
 
       message {
         content_type = "PlainText"
-        content = "At what time would you like to schedule the appointment"
+        content = "What items would you like to order? Please provide a list of items and any special requests."
       }
     }
   }
 }
 
-resource "aws_lex_intent" "cancel_appointment" {
-  name             = "CancelAppointment"
-  description      = "Intent to cancel dental appointments"
-  create_version   = false
+resource "aws_lex_intent" "cancel_order" {
+  name = "CancelOrder"
+  description = "Intent to cancel a food order"
+  create_version = false
 
   sample_utterances = [
-    "I want to cancel my appointment.",
-    "Cancel my dentist appointment.",
-    "I need to reschedule my dental checkup.",
-  ]
+    "I want to cancel my order.",
+    "Cancel my food order.",
+    "I need to change my order.",
+    "Can I modify my order?",
+    "Is it possible to cancel my food delivery?",
+    "I'd like to change my order details.",
+    "I changed my mind, I want to cancel my order.",
+    "How can I update my order?",
+    "I have a change in my food order.",
+    "Is it too late to cancel my order?"
+]
 
   confirmation_prompt {
     max_attempts = 2
 
     message {
       content_type = "PlainText"
-      content = "Of course, I can assist you with canceling your appointment. Please provide the appointment reference or the date and time of your appointment."
+      content = "Of course, I can assist you with canceling your order. Please provide the order number or any relevant details."
     }
   }
 
@@ -146,42 +142,49 @@ resource "aws_lex_intent" "cancel_appointment" {
   }
 
   slot {
-    name        = "AppointmentReference"
-    description = "The appointment reference"
-    priority    = 1
-    slot_constraint   = "Required"
-    slot_type         = "AMAZON.NUMBER"
+    name = "OrderNumber"
+    description = "The order number"
+    priority = 1
+    slot_constraint = "Required"
+    slot_type = "AMAZON.NUMBER"
 
-    sample_utterances = ["I want to cancel my appointment with reference {AppointmentReference}"]
+    sample_utterances = ["I want to cancel my order with order number {OrderNumber}"]
 
     value_elicitation_prompt {
       max_attempts = 2
 
       message {
         content_type = "PlainText"
-        content = "Please provide your appointment reference."
+        content = "Please provide your order number or relevant order details."
       }
     }
   }
 }
 
-resource "aws_lex_intent" "get_appointment_details" {
-  name             = "GetAppointmentDetails"
-  description      = "Intent to retrieve appointment details"
-  create_version   = false
+resource "aws_lex_intent" "get_order_status" {
+  name = "GetOrderStatus"
+  description = "Intent to retrieve order status"
+  create_version = false
 
   sample_utterances = [
-    "Tell me about my appointment.",
-    "What's the date of my dentist appointment",
-    "Give me details about my dental checkup.",
-  ]
+    "Tell me about my order.",
+    "What's the status of my food order?",
+    "Give me details about my delivery.",
+    "Can you provide information on my order?",
+    "I'd like to know the status of my delivery.",
+    "Check the status of my food order, please.",
+    "Tell me about my recent order.",
+    "What's the update on my food delivery?",
+    "Can you give me an update on my order?",
+    "I want to track my food order."
+]
 
   confirmation_prompt {
     max_attempts = 2
 
     message {
       content_type = "PlainText"
-      content = "I can help you with that. Please provide your appointment reference or your name, and I will retrieve your appointment details."
+      content = "I can help you with that. Please provide your order number or your name, and I will retrieve your order status."
     }
   }
 
@@ -197,20 +200,31 @@ resource "aws_lex_intent" "get_appointment_details" {
   }
 
   slot {
-    name        = "AppointmentReference"
-    description = "The appointment reference"
-    priority    = 1
-    slot_constraint   = "Required"
-    slot_type         = "AMAZON.NUMBER"
+    name = "OrderNumber"
+    description = "The order number"
+    priority = 1
+    slot_constraint = "Required"
+    slot_type = "AMAZON.NUMBER"
 
-    sample_utterances = ["Tell me about my appointment with reference {AppointmentReference}"]
+    sample_utterances = [
+    "Tell me about my order with order number {OrderNumber}",
+    "What's the status of my order with order number {OrderNumber}?",
+    "Give me details about my delivery with order number {OrderNumber}.",
+    "I'd like to check the status of my order with order number {OrderNumber}.",
+    "Can you provide information about my order with order number {OrderNumber}?",
+    "Check my order status for order number {OrderNumber}.",
+    "I want to know the status of my order with order number {OrderNumber}.",
+    "Please give me updates on my order with order number {OrderNumber}.",
+    "What can you tell me about my order with order number {OrderNumber}?",
+    "Provide me with the status of my order with order number {OrderNumber}."
+]
 
     value_elicitation_prompt {
       max_attempts = 2
 
       message {
         content_type = "PlainText"
-        content = "Please provide your appointment reference or your name."
+        content = "Please provide your order number or your name."
       }
     }
   }
